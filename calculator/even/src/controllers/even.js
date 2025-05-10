ec78f7af06bae8d4042b36df8f767aaba83a1c29
ashwith <24ca0010.ashwith@sjec.ac.in>
2025-05-10 16:20:44 +0530

    update invalid auth or token expiration

--- calculator/even/src/controllers/even.js
+++ calculator/even/src/controllers/even.js
@@ -12,24 +12,41 @@ const addSetItem = (value) => {
             currentSet.delete(currentSet.values().next().value);
         }
         currentSet.add(value);
-        avg+=value;
+        avg += value;
     }
 }
 
 const EvenSet = async (req, res) => {
     try {
 
-
-        const decoded = jwt.decode(token);
-
-        const response = await axios.get(`${GetNumsApi}`, { timeout: 500 }, {
+        let response = await axios.get(`${GetNumsApi}`, { timeout: 500 }, {
             headers: {
-                'Authorization': `${token}`,
+                'Authorization': `Bearer ${token}`,
             }
         });
 
         if (!response) {
             return res.status(400).json({ message: "Could not get the numbers from external API" });
+        }
+
+        if (response.status == 401 && response.data.message === 'Invalid authorization token') {
+            const getToken = axios.post(`http://20.244.56.144/evaluation-service/auth`, {
+                "email": "24ca0010.ashwith@sjec.ac.in",
+                "name": "ashwith a salian",
+                "rollNo": "4so24mc010",
+                "accessCode": "KjJAxP",
+                "clientID": "389860ef-3720-4bb4-a189-ec31bb181cae",
+                "clientSecret": "hXVQzYZahbqUvjYV"
+            });
+
+            token = getToken.data.access_token;
+
+            response = axios.get(`${GetNumsApi}`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                }
+            });
+
         }
 
         const newNums = response.body.numbers;
@@ -40,7 +57,7 @@ const EvenSet = async (req, res) => {
             addSetItem(element);
         });
 
-        return res.status(200).json({windowPrevState: currentoldSet, windowCurrState: newNums, numbers: [...currentSet], avg: avg/currentSet.size});
+        return res.status(200).json({ windowPrevState: currentoldSet, windowCurrState: newNums, numbers: [...currentSet], avg: avg / currentSet.size });
 
     } catch (error) {
         console.error("Error while functionName:", error);
@@ -48,4 +65,4 @@ const EvenSet = async (req, res) => {
     }
 }
 
-module.exports = {EvenSet}
+module.exports = { EvenSet }
